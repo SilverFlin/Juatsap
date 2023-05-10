b3d4f2043e0b0defa1dce931fb2705eee87386c4
SilverFlin <toledorusso@outlook.com>
2023-05-10 09:52:15 -0700

    chore: init project

---
+++ 03_Implementation/Juatsapp/src/main/java/org/itson/juatsapp/Juatsapp.java
@@ -0,0 +1,12 @@
+package org.itson.juatsapp;
+
+/**
+ *
+ * @author Toled
+ */
+public class Juatsapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
