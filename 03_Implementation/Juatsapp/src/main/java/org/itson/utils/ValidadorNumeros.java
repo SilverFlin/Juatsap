e93defc23bd0fa1296582e212891bd11da3eed94
SilverFlin <toledorusso@outlook.com>
2023-05-15 09:53:29 -0700

    feat: validar numeros

---
+++ 03_Implementation/Juatsapp/src/main/java/org/itson/utils/ValidadorNumeros.java
@@ -0,0 +1,39 @@
+package org.itson.utils;
+
+/**
+ *
+ * @author Toled
+ */
+public class ValidadorNumeros {
+
+    private ValidadorNumeros() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static boolean isPositivo(Double valor) {
+        return !isNull(valor) && valor > 0;
+    }
+
+    public static <T> boolean isNull(T elemento) {
+        return elemento == null;
+    }
+
+    public static boolean isDouble(String valor) {
+        try {
+            Double.valueOf(valor);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isInteger(String valor) {
+        try {
+            Integer.valueOf(valor);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+}
