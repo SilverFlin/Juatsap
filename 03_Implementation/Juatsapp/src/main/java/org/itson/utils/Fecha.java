9d89400890f073a97ba0ebbff18f58b03616a77e
Brawun <116768470+Brawun@users.noreply.github.com>
2023-05-18 00:09:05 -0700

    fix: clase fecha

--- 03_Implementation/Juatsapp/src/main/java/org/itson/utils/Fecha.java
+++ 03_Implementation/Juatsapp/src/main/java/org/itson/utils/Fecha.java
@@ -5,10 +5,8 @@ package org.itson.utils;
 
 // Importaciones
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Esta clase permite encapsular herramientas útiles a la hora de querer
@@ -17,60 +15,61 @@ import java.util.GregorianCalendar;
 public class Fecha {
 
     /**
+     * Método para obtener la fecha en tipo LocalDateTime del preciso momento.
+     *
+     * @return ahora, fecha y hora del preciso momento.
+     */
+    public LocalDateTime fechaAhora() {
+        return LocalDateTime.now();
+    }
+    
+    /**
+     * Método para obtener la fecha y hora del preciso momento.
+     *
+     * @return ahora, fecha y hora del preciso momento.
+     */
+    public String fechaAhoraConHora() {
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return ahora.format(formatter);
+    }
+    
+    /**
      * Método para obtener la fecha del preciso momento.
      *
      * @return ahora, fecha del preciso momento.
      */
-    public Calendar fechaAhora() {
-        Calendar ahora = Calendar.getInstance();
-        ahora.setTime(new Date());
-        return ahora;
+    public String fechaAhoraSinHora() {
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return ahora.format(formatter);
     }
 
     /**
-     * Método que toma un objeto Calendar y devuelve una cadena de texto que
-     * representa la fecha en un formato específico "yyyy/MM/dd", que representa
-     * el año, el mes y el día en el orden especificado.
+     * Método que toma un fecha tipo LocalDateTime y regresa un string que incluye
+     * la fecha, horas y minutos de dicha fecha.
      *
-     * @param fecha Ojeto tipo Calendar que regresará como string.
-     * @return Fecha representada en format yyyy/MM/dd en un string.
+     * @param fecha Ojeto tipo LocalDateTime que regresará como string.
+     * @return Fecha representada en format dd/MM/yyyy HH:mm en un string.
      * @throws ParseException en caso que haya una excepción en los tipos de
      * datos.
      */
-    public String formatoFecha(Calendar fecha) throws ParseException {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
-        return formatter.format(fecha.getTime());
-    }
-
-    /**
-     * Método que recibe dos objetos de tipo Calendar, fechaInicial y
-     * fechaFinal, que representan las fechas a comparar. La función de este
-     * método es calcular la diferencia en días entre las dos fechas y
-     * retornarla como un entero.
-     *
-     * @param fechaInicial Fecha inicial
-     * @param fechaFinal Fecha final
-     * @return diferenciaDias entero con la diferencia de días entre fecha
-     * inicial y final
-     */
-    public Integer calcularDiferenciaDias(Calendar fechaInicial, Calendar fechaFinal) {
-        // Convertir las fechas a milisegundos
-        long milisegundosFechaInicial = fechaInicial.getTimeInMillis();
-        long milisegundosFechaFinal = fechaFinal.getTimeInMillis();
-
-        // Calcular la diferencia en milisegundos
-        long diferenciaMilisegundos = milisegundosFechaFinal - milisegundosFechaInicial;
-
-        // Convertir la diferencia de milisegundos a días
-        int diferenciaDias = (int) (diferenciaMilisegundos / (24 * 60 * 60 * 1000));
-
-        // Retornar la diferencia en días
-        return diferenciaDias;
+    public String formatoFechaConHora(LocalDateTime fecha) throws ParseException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return fecha.format(formatter);
     }
     
-    public Calendar crearFecha(Integer dia, Integer mes, Integer anho) {
-        // Se desplaza el mes -1
-        mes = mes - 1;
-        return new GregorianCalendar(anho, mes, dia);
+    /**
+     * Método que toma un fecha tipo LocalDateTime y regresa un string que incluye
+     * la fecha.
+     *
+     * @param fecha Ojeto tipo LocalDateTime que regresará como string.
+     * @return Fecha representada en format dd/MM/yyyy en un string.
+     * @throws ParseException en caso que haya una excepción en los tipos de
+     * datos.
+     */
+    public String formatoFechaSinHora(LocalDateTime fecha) throws ParseException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return fecha.format(formatter);
     }
 }
